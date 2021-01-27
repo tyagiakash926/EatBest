ab07d7fd0c00a351af2607f161200337270123bb
Akash Tyagi <tyagiakash926@gmail.com>
2021-01-27 16:35:27 +0530

    1st commit

--- Controller/planController.js
+++ Controller/planController.js
@@ -1,4 +1,3 @@
-const { error } = require("jquery");
 const planModel = require("../Model/plansModel");
 
 
--- Controller/userController.js
+++ Controller/userController.js
@@ -1,4 +1,3 @@
-const { error } = require("jquery");
 const userModel = require("../Model/usersModel");
 
 
