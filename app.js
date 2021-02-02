13a5825f0e7986237b51c4f63afa99d4f0ef082e
Akash Tyagi <tyagiakash926@gmail.com>
2021-02-02 17:55:27 +0530

    1st commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const cors = require('cors');
+const cors = require('cors');
 const planRouter = require("./Router/planRouter");
 const userRouter  = require("./Router/userRouter");
 const { json } = require("express");
