ee67e726528519416fa6d2d5cb4cd3bab7dba615
Akash Tyagi <tyagiakash926@gmail.com>
2021-02-02 18:42:15 +0530

    1st commit

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const cookieParser = require("cookie-parser");
 const bookingRouter = require("./Router/bookingRouter");
 
 app.all('*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'URLs to trust of allow');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   if ('OPTIONS' == req.method) {
