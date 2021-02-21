462b77f71b7d9600138701e623c8ef050cf455a1
Akash Tyagi <tyagiakash926@gmail.com>
2021-02-21 23:48:05 +0530

    home page media query done

--- public/js/script.js
+++ public/js/script.js
@@ -27,6 +27,25 @@ window.addEventListener("load" , function(){
 }
 })
 
+let humberburger = document.querySelector(".header-nav-bar-icon img");
+let resul=document.querySelector(".showcase-ul");
+let headerres=document.querySelector(".header-nav-bar");
+humberburger.addEventListener("click",function(){
+  
+
+  if(headerres.classList.contains("height-resp")){
+    headerres.classList.remove("height-resp");
+    document.querySelector(".header-nav-bar-icon").classList.remove("nav-res-icon");
+    resul.classList.add("vclass-resp");
+  }else {
+    headerres.classList.add("height-resp");
+    resul.classList.remove("vclass-resp");
+    document.querySelector(".header-nav-bar-icon").classList.add("nav-res-icon");
+  }
+
+
+})
+
 let buynow_btn= document.querySelector(".buynow-btn");
 let explore_btn = document.querySelector(".explore-btn");
 let discount_template_btn=document.querySelector(".discount-template-btn");
